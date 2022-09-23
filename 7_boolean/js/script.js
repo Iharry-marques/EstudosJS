892315cd0912569ba6ee00b4bbb4e6d0f15199f5
Fabio Marques Melo de Oliveira <fmmoliveira@al.casperlibero.edu.br>
2022-09-23 11:29:09 -0300

    novaatt3

---
+++ 7_boolean/js/script.js
@@ -0,0 +1,25 @@
+// Tipo de dados: Boolean
+
+/*
+    * O boolean representa um valor verdadeiro ou falso;
+    * Comparações resultam em booleans;
+    * Podemos também atribuir um true ou false para variável  e ela terá o tipo de Boolean;
+*/
+
+var verdadeiro = true;
+
+console.log(verdadeiro)
+console.log(typeof verdadeiro)
+
+console.log(typeof true)
+console.log(typeof false)
+
+var falso = false;
+
+console.log(false)
+console.log(typeof false)
+
+// alguns nomes sao reservador para outras finalidades como por exemplo:  function, false, var...
+
+// exemplo = var function = 3;   xxxxx
+
---
+++ 8_null_and_undefined/js/script.js
@@ -0,0 +1,27 @@
+// undefined e null 
+
+/*
+    * undefined e null também são considerados tipos de dados;
+    * O null é um tipo de dado que representa um valor;
+    * O undefined é um tipo de dado para uma variável com valor não atribuído
+*/
+
+// Hoisting - içamento
+console.log(sobrenome);
+// var numero = 5;
+console.log(numero)
+
+
+var nome = null;
+var sobrenome = 'Marques';
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Fabio"
+
+console.log(nome);
+
+var numero  = 5;
+
+
