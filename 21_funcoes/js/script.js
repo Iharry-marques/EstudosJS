67c6fa9ae670e480400e20ba044b3aafac3fc275
Fabio Marques Melo de Oliveira <fmmoliveira@fcl.com.br>
2022-11-08 13:27:14 -0300

    novaattestudo

--- 21_funcoes/js/script.js
+++ 21_funcoes/js/script.js
@@ -5,42 +5,40 @@
     * A função precisa ser invocada para ser executada
 */
 
-function primeiraFuncao(){
+function primeiraFuncao() {
 
-    console.log("Hello funções")
+    console.log("Hello World das funções");
 
 }
-primeiraFuncao()
+
+primeiraFuncao();
 
 function dizerNome(nome) {
-    console.log("O nome é " + nome)
+    console.log("O nome é: " + nome);
 }
 
 dizerNome("Fabio");
+dizerNome("Pedro");
+dizerNome("Silva");
 
-function idade (idade) {
-    console.log("Você tem " + idade + " anos" )
-}
-
-idade("23");
-idade("17");
-idade("34");
-
-var nomeDoBancoDeDados = "João";
+var nomeDoBancoDeDados = "João"
 
 dizerNome(nomeDoBancoDeDados)
 
-
-function soma(a, b){
+function soma (a, b) {
     var soma = a + b;
-    return a + b;
+    return soma;
 }
 
-var somaUm = soma(2,5)
+var somaUm  = soma(2,5);
 
 console.log(somaUm)
 
-var somaDois = soma(5,5)
-console.log(somaDois)
+var somaDois = soma(5,5);
+
+console.log(somaDois);
 
 console.log(soma(4,5));
+
+
+
---
+++ 22_DOM/js/script.js
@@ -0,0 +1,11 @@
+var lista = ['laranja', 'maçã', 'pera', 'limão' ]
+
+var listaUl = document.createElement('ul');
+
+var body = document.getElementsByTagName('body');
+
+console.log(body[0]);
+
+
+
+
